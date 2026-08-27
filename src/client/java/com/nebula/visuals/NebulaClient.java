e528d6acba7e69869eaddcc70ab352b8315e1db0
letov7482-ui <letov7482@gmail.com>
2026-08-27 18:03:16 +0300

    Create NebulaClient.java

---
+++ src/client/java/com/nebula/visuals/NebulaClient.java
@@ -0,0 +1,46 @@
+package com.nebula.visuals;
+
+import com.nebula.visuals.gui.NebulaScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NebulaClient implements ClientModInitializer {
+
+    public static final String MOD_ID = "nebula";
+    public static final String NAME = "Nebula Visuals";
+
+    public static final Logger LOGGER =
+            LoggerFactory.getLogger(NAME);
+
+    private static KeyMapping openGuiKey;
+
+    @Override
+    public void onInitializeClient() {
+
+        LOGGER.info("Nebula Visuals initialized.");
+
+        openGuiKey = KeyBindingHelper.registerKeyBinding(
+                new KeyMapping(
+                        "key.nebula.open_gui",
+                        GLFW.GLFW_KEY_RIGHT_SHIFT,
+                        "category.nebula"
+                )
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+
+            while (openGuiKey.consumeClick()) {
+
+                if (client.screen == null) {
+                    client.setScreen(new NebulaScreen());
+                }
+            }
+        });
+    }
+}
