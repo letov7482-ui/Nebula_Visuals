257a4d55703c67cfad7e51086ec731c86bbcaa05
letov7482-ui <letov7482@gmail.com>
2026-08-27 18:16:58 +0300

    Create HitEffects.java

---
+++ src/client/java/com/nebula/visuals/module/impl/HitEffects.java
@@ -0,0 +1,15 @@
+package com.nebula.visuals.module.impl;
+
+import com.nebula.visuals.module.Category;
+import com.nebula.visuals.module.Module;
+
+public final class HitEffects extends Module {
+
+    public HitEffects() {
+        super(
+                "Hit Effects",
+                "Custom visual effects when hitting an entity.",
+                Category.VISUALS
+        );
+    }
+}
